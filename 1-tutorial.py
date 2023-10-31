2991d20ecf7803f02e3c2f7db105e429816be041
Peter Vidos <peter.vidos@gmail.com>
2023-10-31 21:41:06 +0100

    Create 1-tutorial.py

---
+++ 1-tutorial.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import streamlit as st
+from streamlit_vizzu import Config, Data, Style, VizzuChart
+
+# Create a VizzuChart object with the default height and width
+chart = VizzuChart()
+
+# Generate some data and add it to the chart
+df = pd.DataFrame({"cat": ["x", "y", "z"], "val": [1, 2, 3]})
+data = Data()
+data.add_df(df)
+chart.animate(data)
+
+st.subheader(
+    "Visit [intro-to-vizzu-in.streamlit.app](https://intro-to-vizzu-in.streamlit.app/) "
+    "to follow along"
+)
+
+# Add some configuration to tell Vizzu how to display the data
+chart.animate(Config({"x": "cat", "y": "val", "title": "Look at my plot!"}))
+
+if st.checkbox("Swap"):
+    chart.animate(Config({"x": "val", "y": "cat", "title": "Swapped!"}))
+
+# Show the chart in the app!
+output = chart.show()
+
+if output is not None and "marker" in output:
+    st.write("value of clicked bar:", output["marker"]["values"]["val"])
+
+st.caption("Data shown on the chart")
+st.dataframe(df)
+
+st.write(
+    "Check the [source code](https://github.com/vizzu-streamlit/streamlit-vizzu) "
+    "of the bidirectional component that makes this possible, created by "
+    "[blackary](https://github.com/blackary)"
+)
